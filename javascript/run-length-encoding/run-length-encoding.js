13c5a35bdca02548fa020f7d73aacebbd3cc895b
Andrew Beverley <andrew@andrewbeverley.co.uk>
2019-09-05 00:23:48 +0100

    start of running length encoding

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,12 +1,29 @@
-//
-// This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/**
+ *
+ * @param strands
+ */
+const encode = myValue => [...myValue].reduce((accumulator,
+                                                currentValue,
+                                                currentIndex,
+                                                array) => {
+counter = 1;
+  if (currentValue === array[currentIndex + 1]) {
+    //increment and move on
+    counter++;
+    return accumulator;
+  } else {
+    //save letter and number
+    accumulator += (currentValue + counter);
+    counter = 1;
+    return accumulator;
+  }
 
-export const encode = () => {
+}, '');
+
+const decode = () => {
   throw new Error("Remove this statement and implement this function");
 };
 
-export const decode = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+
+console.log(encode('AABBBCCCC'));
+console.log(encode('ABC'));
