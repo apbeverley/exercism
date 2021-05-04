fbd556f73b2cc52339f7da2ec1d929e72155f284
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-04 21:02:54 +0100

    queens attack complete

--- php/queen-attack/queen-attack.php
+++ php/queen-attack/queen-attack.php
@@ -1,15 +1,31 @@
 <?php
-
-function placeQueen(int $x, int $y){
     
-    'The rank and file numbers must be positive.'
+    function placeQueen(int $x, int $y): bool
+    {
+        if (($x < 0) || ($y < 0)) {
+            throw new InvalidArgumentException('The rank and file numbers must be positive.');
+        }
+        if (($x > 7) || ($y > 7)) {
+            throw new InvalidArgumentException('The position must be on a standard size chess board.');
+        }
+        
+        return true;
+    }
     
-    'The position must be on a standard size chess board.'
-}
-
-function canAttack(array $blackQueen, array $whiteQueen){
-
-}
-    
-    canAttack([2, 2], [1, 1])
-    
+    function canAttack(array $black, array $white): bool
+    {
+        //Is the queen on the same axis as the other
+        if (($black[0] === $white[0]) || ($black[1] === $white[1])) {
+            $status = true;
+        }
+        
+        if ($black[0] - $black[1] === $white[0] - $white[1]) {
+            $status = true;
+        }
+        
+        if ($black[0] + $black[1] === $white[0] + $white[1]) {
+            $status = true;
+        }
+        
+        return $status ?? false;
+    }
