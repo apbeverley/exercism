ad770c219e39716d7ddb7e4bd22ed122af4826a7
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-10 22:20:39 +0100

    transpose complete

--- php/transpose/transpose.php
+++ php/transpose/transpose.php
@@ -1,1 +1,19 @@
 <?php
+    
+    function transpose(array $input): array
+    {
+        $response = [''];
+        
+        foreach ($input as $index => $line) {
+            $length = strlen($line);
+            for ($x = 0; $x < $length; $x++) {
+                if (!isset($response[$x])) {
+                    $response[$x] = str_pad('', $index);
+                }
+                
+                $response[$x] = str_pad($response[$x], $index).$line[$x];
+            }
+        }
+        
+        return $response;
+    }
