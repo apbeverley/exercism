837cce1338c4d62de007b5dc131cffcd9b602f1d
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-11 20:34:09 +0100

    update luhn solution

--- php/luhn/luhn.php
+++ php/luhn/luhn.php
@@ -1,15 +1,19 @@
 <?php
-
-function isValid($cardNumber): bool
-{
-    $checksum = '';
-    $cardNumber = preg_replace("/\s+/", "", $cardNumber);
-
-    if (is_numeric($cardNumber)) {
+    
+    function isValid($cardNumber): bool
+    {
+        $cardNumber = preg_replace("/\s+/", "", $cardNumber);
+        
+        return is_numeric($cardNumber) && isValidLuhn($cardNumber);
+    }
+    
+    function isValidLuhn(string $cardNumber): bool
+    {
+        $checksum = '';
+        
         foreach (str_split(strrev($cardNumber)) as $i => $d) {
             $checksum .= $i % 2 !== 0 ? $d * 2 : $d;
         }
+        
+        return !empty($checksum) && array_sum(str_split($checksum)) % 10 === 0;
     }
-
-    return !empty($checksum) && array_sum(str_split($checksum)) % 10 === 0;
-}
