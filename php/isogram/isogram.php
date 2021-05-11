3e0b440927da0da2db30a74d7c4ead9dce6912ea
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-11 21:53:04 +0100

    complete isogram solution

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -1,1 +1,10 @@
 <?php
+    
+    function isIsogram(string $word)
+    {
+        $formatWord = mb_strtolower(preg_replace(['/\s+/', '/-/'], '', $word));
+        $uniqueWord = array_unique(mb_str_split($formatWord));
+        $wordAsArray = mb_str_split($formatWord);
+        
+        return (count($uniqueWord) === count($wordAsArray));
+    }
