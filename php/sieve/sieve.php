cf94dfa2deb080b24222201a6578939e1a642900
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-12 21:19:57 +0100

    complete sieve

--- php/sieve/sieve.php
+++ php/sieve/sieve.php
@@ -1,1 +1,27 @@
 <?php
+    
+    function sieve(int $number)
+    {
+        $response = [];
+        
+        if ($number !== 1) {
+            for ($i = 2; $i <= $number; $i++) {
+                if (isPrime($i)) {
+                    $response[] = $i;
+                }
+            }
+        }
+        
+        return $response;
+    }
+    
+    function isPrime(int $n): bool
+    {
+        for ($x = 2; $x < $n; $x++) {
+            if ($n % $x === 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
