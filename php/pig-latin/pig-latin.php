85a5b6b20e5cca902fd8a38e420842b03e1f5e33
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-15 21:51:25 +0100

    complete pig latin exercise

--- php/pig-latin/pig-latin.php
+++ php/pig-latin/pig-latin.php
@@ -1,1 +1,16 @@
 <?php
+    
+    function translate(string $word)
+    {
+        $words = explode(" ", $word);
+        $response = [];
+        
+        foreach ($words as $w) {
+            $response[] = preg_replace(
+                    "/^(ch|s?qu|thr?|sch|[^aeiou])(?!t)(?!r)(.+)/",
+                    "$2$1", trim($w)
+                ).'ay';
+        }
+        
+        return implode(" ", $response);
+    }
