20dfa2aa1e5c87d1973e08dfdf168c62404f6fdf
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-20 22:17:22 +0100

    complete series exercise

--- php/series/series.php
+++ php/series/series.php
@@ -1,1 +1,25 @@
 <?php
+    
+    /**
+     * @throws Exception
+     */
+    function slices(string $digits, int $length): array
+    {
+        $response = [];
+        
+        $maxDigits = strlen($digits);
+        
+        if ($length === 0 || $length > $maxDigits) {
+            throw new Exception('invalid length');
+        }
+        
+        for ($x = 0; $x <= $maxDigits; $x++) {
+            if ($x + $length > $maxDigits) {
+                break;
+            }
+            
+            $response[] = substr($digits, $x, $length);
+        }
+        
+        return $response;
+    }
