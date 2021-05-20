658931fc3c5045e04b7fb3943052962ed2509aeb
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-05-20 22:03:24 +0100

    complete Collatz Conjecture exercise

--- php/collatz-conjecture/collatz-conjecture.php
+++ php/collatz-conjecture/collatz-conjecture.php
@@ -1,1 +1,24 @@
 <?php
+    
+    /**
+     * @throws InvalidArgumentException
+     */
+    function steps(int $n): int
+    {
+        if ($n <= 0) {
+            throw new InvalidArgumentException('Only positive numbers are allowed');
+        }
+        
+        $step = 0;
+        
+        while ($n !== 1) {
+            if ($n % 2 === 0) {
+                $n /= 2;
+            } else {
+                $n = ($n * 3) + 1;
+            }
+            $step++;
+        }
+        
+        return $step;
+    }
