b34b6c7b026b44143b9eb84335f24fd5a23dce62
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-06-13 21:55:48 +0100

    robot name complete

--- php/robot-name/robot-name.php
+++ php/robot-name/robot-name.php
@@ -1,1 +1,55 @@
 <?php
+    
+    class Robot
+    {
+        /**
+         * @var string
+         */
+        private string $robotName;
+        
+        /**
+         * @var array|string
+         */
+        private static $usedNames = [];
+        
+        /**
+         * @throws Exception
+         */
+        public function getName (): string
+        {
+            if ( empty($this->robotName) ) {
+                $this->robotName = $this->nameFactoryRobot();
+            }
+            
+            return $this->nameFactoryRobot();
+        }
+        
+        /**
+         * @throws Exception
+         */
+        private function nameFactoryRobot (): string
+        {
+            $robotName = $this->generateRobotName();
+            
+            if ( isset(self::$usedNames[$robotName]) ) {
+                $robotName = $this->nameFactoryRobot();
+            } else {
+                self::$usedNames[$robotName] = true;
+            }
+            
+            return $robotName;
+        }
+        
+        /**
+         * @throws Exception
+         */
+        private function generateRobotName (): string
+        {
+            return chr(random_int(65, 90)).chr(random_int(65, 90)).random_int(100, 999);
+        }
+        
+        public function reset (): void
+        {
+            $this->robotName = '';
+        }
+    }
