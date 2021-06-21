b31cb44071e698f9121f82c9208d2d1800451898
Andrew Beverley <andrew@andrewbeverley.co.uk>
2021-06-21 21:44:53 +0100

    acronym solution

--- php/acronym/acronym.php
+++ php/acronym/acronym.php
@@ -1,1 +1,11 @@
 <?php
+    
+    function acronym ( string $phrase ): string
+    {
+        $acronym = implode('', array_map(
+            static function ( $word ) {
+                return mb_strtoupper($word[0]);
+            }, preg_split('/( |-)/', $phrase)));
+        
+        return strlen($acronym) > 1 ? $acronym : '';
+    }
